bc856f377bf81e56e20722543516b286219414ef
steven-liu-engie <steven.liu@engie.com>
2016-06-05 20:58:43 +0100

    added missing files in git - not sure why dropped

---
+++ BlogStorage/Implementations/BlobStorageHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogStorage.Constants;
+using BlogStorage.Interfaces;
+using log4net;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlogStorage.Implementations
+{
+    public class BlobStorageHandler : ServiceBase, IBlobStorageHandler
+    {
+        private ILog Log { get; }
+
+        public BlobStorageHandler(ILog log) : base(log)
+        {
+            Log = log;
+        }
+
+        public void LogDummyInfo()
+        {
+            LogManagedInfo("Steven Liu", "Dummy Info", "User Create", DateTime.UtcNow.Second,"Create User","Additional message blah blah blah" );
+        }
+
+        public void LogDummyException()
+        {
+            var ex = new Exception("Dummy exception");
+            Log.Error(ex);
+        }
+    }
+}
