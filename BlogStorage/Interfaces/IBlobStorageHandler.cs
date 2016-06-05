1cad5899ce1aa3e6abbb755eb676759741b89de3
Steven Liu <xian.f.liu@gmail.com>
2016-06-05 19:25:51 +0100

    Removed common project

---
+++ BlogStorage/Interfaces/IBlobStorageHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlogStorage.Interfaces
+{
+    public interface IBlobStorageHandler
+    {
+        CloudBlobContainer GetBlobContainer ();
+    }
+}
